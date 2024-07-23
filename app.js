a759691b17cbb89dee4d68fefd2d8f9cdad27a50
er-akshaybrata <akshaybratachakrabarty@gmail.com>
2024-07-23 16:56:27 +0530

    this this staging for es6

---
+++ app.js
@@ -0,0 +1,40 @@
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+const url = import.meta.url
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(path.dirname(url), 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(path.dirname(url), 'public')));
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+ res.send(err.message)
+});
+
+// module.exports = app;
+export default app
